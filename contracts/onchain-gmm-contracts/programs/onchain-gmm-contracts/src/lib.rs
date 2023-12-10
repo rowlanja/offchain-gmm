45d41a7a12d2c666c6cf764d2bc4433aefdeb11c
j r <rowlanja@tcd.ie>
2023-12-10 21:18:13 +0000

    added entrance to deposit

--- contracts/onchain-gmm-contracts/programs/onchain-gmm-contracts/src/lib.rs
+++ contracts/onchain-gmm-contracts/programs/onchain-gmm-contracts/src/lib.rs
@@ -20,7 +20,7 @@ pub mod onchain_gmm_contracts {
     }
 
     pub fn create_pool(
-        ctx: Context<DepositLiquidity>
+        ctx: Context<CreateLiquidityPool>
     ) -> Result<()> {
         let pool = &mut ctx.accounts.application_state;
         pool.token_a_depositors = Vec::new();
@@ -40,6 +40,58 @@ pub mod onchain_gmm_contracts {
 #[derive(Accounts)]
 #[instruction(application_idx: u64, deposit_bump: u8, wallet_bump: u8)]
 pub struct DepositLiquidity<'info> {
+    //  // Derived PDAs
+    //  #[account(
+    //     init,
+    //     payer = user_sending,
+    //     seeds=[b"state".as_ref(), mint_of_token_being_sent_a.key().as_ref(), mint_of_token_being_sent_b.key().as_ref(), application_idx.to_le_bytes().as_ref()],
+    //     bump,
+    //     space = 8 + 32 + 32 + 8 + 70 + 70
+    // )]
+    // application_state: Account<'info, Pool>,
+    // #[account(
+    //     init,
+    //     payer = user_sending,
+    //     seeds=[b"pool_wallet_token_a".as_ref(), mint_of_token_being_sent_a.key().as_ref(), application_idx.to_le_bytes().as_ref()],
+    //     bump,
+    //     token::mint=mint_of_token_being_sent_a,
+    //     token::authority=application_state,
+    // )]
+    // pool_wallet_token_a: Account<'info, TokenAccount>,
+
+    // #[account(
+    //     init,
+    //     payer = user_sending,
+    //     seeds=[b"pool_wallet_token_b".as_ref(), mint_of_token_being_sent_b.key().as_ref(), application_idx.to_le_bytes().as_ref()],
+    //     bump,
+    //     token::mint=mint_of_token_being_sent_b,
+    //     token::authority=application_state,
+    // )]
+    // pool_wallet_token_b: Account<'info, TokenAccount>,
+
+    // // Users and accounts in the system
+    // #[account(mut)]
+    // user_sending: Signer<'info>,                     // Alice
+    // user_receiving: AccountInfo<'info>,              // Bob
+    // mint_of_token_being_sent_a: Account<'info, Mint>,  // USDC
+    // mint_of_token_being_sent_b: Account<'info, Mint>,  // ETH
+    // // Alice's USDC wallet that has already approved the escrow wallet
+    // // #[account(
+    // //     mut,
+    // //     constraint=wallet_to_withdraw_from.owner == user_sending.key(),
+    // //     constraint=wallet_to_withdraw_from.mint == mint_of_token_being_sent.key()
+    // // )]
+    // // wallet_to_withdraw_from: Account<'info, TokenAccount>,
+
+    // // Application level accounts
+    // system_program: Program<'info, System>,
+    // token_program: Program<'info, Token>,
+    // rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+#[instruction(application_idx: u64, deposit_bump: u8, wallet_bump: u8)]
+pub struct CreateLiquidityPool<'info> {
      // Derived PDAs
      #[account(
         init,
