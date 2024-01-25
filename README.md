6de7c22a25a15261697847f490426b1c8d309fd8
j r <rowlanja@tcd.ie>
2024-01-25 18:10:00 +0000

    m

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # offchain-gmm
+
+use 'cat .anchor/program-logs/*' to get logs
+
+https://www.anchor-lang.com/docs/space
+
+https://ethereum.org/en/developers/tutorials/uniswap-v2-annotated-code/#uniswapv2pair
+
+https://github.com/osmosis-labs/osmosis/blob/main/x/concentrated-liquidity/README.md
+
+https://uniswapv3book.com/milestone_1/calculating-liquidity.html
+
+https://github.com/quasar-finance/quasar/blob/aa72bb91b2ee9957b92d85b271e078b00ce264d2/smart-contracts/contracts/cl-vault/src/math/tick.rs#L130
+
+https://github.com/CycanTech/Cyswap/blob/master/swap/contracts/core/pool/lib.rs
--- contracts/onchain-gmm-contracts/tests/onchain-gmm-contracts.ts
+++ contracts/onchain-gmm-contracts/tests/onchain-gmm-contracts.ts
@@ -2,7 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import * as utils from "./utils";
 import * as spl from '@solana/spl-token';
 import { Program } from "@coral-xyz/anchor";
-import { PublicKey } from '@solana/web3.js';
+import { PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js';
 import { TOKEN_PROGRAM_ID, Token } from "@solana/spl-token";
 import { OnchainGmmContracts } from "../target/types/onchain_gmm_contracts";
 import { expect } from 'chai';
@@ -30,8 +30,8 @@ describe("onchain-gmm-contracts", () => {
   let alice: anchor.web3.Keypair;
   let aliceWallet: anchor.web3.PublicKey;
   let bob: anchor.web3.Keypair;
-
-  let pda: PDAParameters;
+  let solTokenAddress: anchor.web3.Keypair;
+  let solRewardsPoolAddress: anchor.web3.Keypair;
 
   beforeEach(async () => {
     mintAddress = await createMint(provider.connection);
@@ -39,9 +39,7 @@ describe("onchain-gmm-contracts", () => {
 
     let _rest;
     [bob, ..._rest] = await createUserAndAssociatedWallet(provider.connection);
-
-    pda = await getPdaParams(provider.connection, alice.publicKey, bob.publicKey, mintAddress);
-});
+  });
 
 
   // it("creates liquidity pool!", async () => {
@@ -49,7 +47,24 @@ describe("onchain-gmm-contracts", () => {
     const [poolStatePDA, poolBump] = await PublicKey.findProgramAddress(
       [
         anchor.utils.bytes.utf8.encode('pool-state'),
-        alice.publicKey.toBuffer(),
+        mintAddress.toBuffer(),
+        mintAddress.toBuffer(),
+      ],
+      program.programId
+    )
+
+    const [solToken0StatePDA, solToken0StateBump] = await PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode('sol'),
+        mintAddress.toBuffer(),
+      ],
+      program.programId
+    )
+
+    const [solToken1StatePDA, solToken1StateBump] = await PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode('sol'),
+        mintAddress.toBuffer(),
       ],
       program.programId
     )
@@ -156,6 +171,22 @@ describe("onchain-gmm-contracts", () => {
       program.programId
     )
 
+    const [poolWalletTokenBPDASol, walletSolTokenBBump] = await PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode('pool_token_wallet'),
+        mintAddress.toBuffer()
+      ],
+      program.programId
+    )
+
+    const [poolWalletTokenAPDASol, walletSolTokenABump] = await PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode('pool_token_wallet'),
+        mintAddress.toBuffer()
+      ],
+      program.programId
+    )
+
     const [poolWalletTokenBPDA, walletTokenBBump] = await PublicKey.findProgramAddress(
       [
         anchor.utils.bytes.utf8.encode('pool_wallet_token_1'),
@@ -169,6 +200,24 @@ describe("onchain-gmm-contracts", () => {
         anchor.utils.bytes.utf8.encode('position'),
         alice.publicKey.toBuffer(),
         mintAddress.toBuffer()
+      ],
+      program.programId
+    )
+
+    const [userSolToken0StakePDA, userSolToken0StakeBump] = await PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode('position'),
+        alice.publicKey.toBuffer(),
+        solToken0StatePDA.toBuffer()
+      ],
+      program.programId
+    )
+
+    const [userSolToken1StakePDA, userSolToken1StakeBump] = await PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode('position'),
+        alice.publicKey.toBuffer(),
+        solToken1StatePDA.toBuffer()
       ],
       program.programId
     )
@@ -189,7 +238,7 @@ describe("onchain-gmm-contracts", () => {
     let tokenASwapAmount = new anchor.BN(5);
 
     await program.methods
-    .createPool(tokenADepositAmount, tokenBDepositAmount, alice.publicKey)
+    .createPool(tokenADepositAmount, tokenBDepositAmount, alice.publicKey, false)
     .accounts({
       user: alice.publicKey,
       poolState: poolStatePDA,
@@ -201,6 +250,57 @@ describe("onchain-gmm-contracts", () => {
       userWalletToken1: aliceWallet,
       token0Mint: mintAddress,
       token1Mint: mintAddress,
+      tokenProgram: spl.TOKEN_PROGRAM_ID
+    })
+    .signers([alice])
+    .rpc();
+
+    // SET UP TOKEN0_SOL pool
+    console.log("[PRE] setting up SOL token0 Pool");
+    await program.methods
+    .createSolPool(new anchor.BN(1000000), new anchor.BN(100))
+    .accounts({
+      user: alice.publicKey,
+      poolState: solToken0StatePDA,
+      poolTokenWallet: poolWalletTokenAPDASol,
+      position: userSolToken0StakePDA,
+      wallet: alice.publicKey,
+      userWalletToken: aliceWallet,
+      tokenMint: mintAddress,
+      tokenProgram: spl.TOKEN_PROGRAM_ID
+    })
+    .signers([alice])
+    .rpc();
+
+    console.log("[PRE] setting up SOL token1 Pool");
+    await program.methods
+    .createSolPool(new anchor.BN(1000000), new anchor.BN(200))
+    .accounts({
+      user: alice.publicKey,
+      poolState: solToken1StatePDA,
+      poolTokenWallet: poolWalletTokenAPDASol,
+      position: userSolToken1StakePDA,
+      wallet: alice.publicKey,
+      userWalletToken: aliceWallet,
+      tokenMint: mintAddress,
+      tokenProgram: spl.TOKEN_PROGRAM_ID
+    })
+    .signers([alice])
+    .rpc();
+    let [, poolBalanceSolPoolTokenA] = await readAccount(poolWalletTokenAPDASol, provider);
+    console.log("[POST] Pool Balance Token A / SOL : " + poolBalanceSolPoolTokenA);
+
+    // SET UP TOKEN1_SOL pool
+    await program.methods
+    .createSolPool(new anchor.BN(1), new anchor.BN(100))
+    .accounts({
+      user: alice.publicKey,
+      poolState: solToken0StatePDA,
+      poolTokenWallet: poolWalletTokenBPDASol,
+      position: userSolToken0StakePDA,
+      wallet: alice.publicKey,
+      userWalletToken: aliceWallet,
+      tokenMint: mintAddress,
       tokenProgram: spl.TOKEN_PROGRAM_ID
     })
     .signers([alice])
@@ -219,12 +319,19 @@ describe("onchain-gmm-contracts", () => {
     console.log("amount : " + state.amount.toString());
     console.log("timestamp : " + state.timestamp.toString());
 
+
+    let poolPDA = await program.account.pool.fetch(poolStatePDA);
+    console.log("Pool PDA token 0 balance : " + poolPDA.totalStakedToken0.toString());
+    console.log("Pool PDA token 1 balance : " + poolPDA.totalStakedToken1.toString());
+
     console.log("TIME TO SWAP ");
     await program.methods
     .swap(tokenASwapAmount, false)
     .accounts({
       user: alice.publicKey,
       pool: poolStatePDA,
+      // rewardPool0For2: solToken0StatePDA,
+      // rewardPool1For2: solToken1StatePDA,
       poolWalletToken0: poolWalletTokenAPDA,
       poolWalletToken1: poolWalletTokenBPDA,
       stakersList: stakeListPDA,
@@ -237,11 +344,9 @@ describe("onchain-gmm-contracts", () => {
     .signers([alice])
     .rpc();
 
-    [, poolBalancePreTokenA] = await readAccount(poolWalletTokenAPDA, provider);
-    console.log("[POST] Pool Balance Token A : " + poolBalancePreTokenA);
-
-    [, poolBalancePreTokenA] = await readAccount(poolWalletTokenBPDA, provider);
-    console.log("[POST] Pool Balance Token B : " + poolBalancePreTokenA);
+    poolPDA = await program.account.pool.fetch(poolStatePDA);
+    console.log("Pool PDA token 0 balance : " + poolPDA.totalStakedToken0.toString());
+    console.log("Pool PDA token 1 balance : " + poolPDA.totalStakedToken1.toString());
 
     state = await program.account.position.fetch(userStakePDA);
     console.log("amount : " + state.amount.toString());
@@ -258,7 +363,7 @@ describe("onchain-gmm-contracts", () => {
     txFund.add(anchor.web3.SystemProgram.transfer({
         fromPubkey: provider.wallet.publicKey,
         toPubkey: user.publicKey,
-        lamports: 5 * anchor.web3.LAMPORTS_PER_SOL,
+        lamports: 500 * anchor.web3.LAMPORTS_PER_SOL,
     }));
     const sigTxFund = await provider.sendAndConfirm(txFund);
     console.log(`[${user.publicKey.toBase58()}] Funded new account with 5 SOL: ${sigTxFund}`);
